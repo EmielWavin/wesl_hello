5a2613f162cea3db3f8c779a0c54975fa3497a40
EmielWavin <emiel.kosse@wavin.com>
2024-11-27 09:35:03 +0100

    Adding initial structure to repo

---
+++ src/main.c
@@ -0,0 +1,13 @@
+/* WAVIN T&I Application framework example
+
+DATE:       27-NOV-2024
+AUTHOR:     E.Kosse
+
+VERSION:    0.01
+*/
+#include <stdio.h>
+
+int main() {
+    printf("Hello, wesl!\n");
+    return 0;
+}
